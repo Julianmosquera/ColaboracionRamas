37e0873ad8aaf3fbdbef63d48b01a435d256c45c
ylagorebollar <ylagorebollar@atanasoff03.danielcastelao.org>
2016-02-29 09:31:05 +0100

    Metodos camion con herencia

--- src/colaboracionramas/Camion.java
+++ src/colaboracionramas/Camion.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package colaboracionramas;
 
 /**
  *
  * @author julianlinux
  */
-public class Camion {
+public class Camion extends Vehiculo{
+    private int nRodas;
+
+    public Camion() {
+        super();
+    }
+
+    public Camion(int cilindrada, int potencia, int nRodas) {
+        super(cilindrada, potencia);
+        this.nRodas=nRodas;
+    }
+
+    public int getnRodas() {
+        return nRodas;
+    }
+
+    public void setnRodas(int nRodas) {
+        this.nRodas = nRodas;
+    }
+    public float calcularImposto(){
+        return (getCilindrada()/30)+(getPotencia()*20)+(nRodas*20);
+    }
+
+    @Override
+    public String toString() {
+        return "Camion{" + "nRodas=" + nRodas + '}';
+    }
     
+}
 }
