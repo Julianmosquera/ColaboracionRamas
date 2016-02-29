272fbf3972f924f1beabfd320fa55e6b8ecede3a
julianlinux <julianlinux@julianlinux-X555LA>
2016-02-29 09:41:04 +0100

    Clase coche

--- src/colaboracionramas/Coche.java
+++ src/colaboracionramas/Coche.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package colaboracionramas;
 
 /**
  *
  * @author julianlinux
  */
-public class Coche {
-    
+public class Coche extends Vehiculo {
+
+    boolean nivel_aceite = true;
+
+    public Coche(String modelo, String marca, int cilindrada, int numero_rudas) {
+        super(modelo, marca, cilindrada, numero_rudas);
+    }
+
+    public void comprobarAceite() {
+        if (nivel_aceite = true) {
+            System.out.println("Correcto");
+
+        } else {
+            System.out.println("Taller");
+        }
+
+    }
+
 }
